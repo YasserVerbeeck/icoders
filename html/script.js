71c20f953d62e843c4386c22ac8a5d292b4c977a
eliaselmokadem <s141765@ap.be>
2023-03-27 20:11:23 +0200

    script beter verwerkt

--- html/script.js
+++ html/script.js
@@ -1,24 +1,55 @@
 var ondernemingsnummersAPI = [
 	{ 
-		ondernemingsnummer1: "test1",
-		ondernemingsnummer2: "test2"
+		ondernemingsnummer: "test1",
 	},
+    {
+        ondernemingsnummer: "test2"
+    },
+    {
+        ondernemingsnummer: "BE123"
+    },
 	{ 
-		ondernemingsnummer1: "test2",
-		ondernemingsnummer2: "test1"
-	}
+		ondernemingsnummer: "BE456"
+	},
+    { 
+		ondernemingsnummer: "BE789"
+	},
+    {
+        ondernemingsnummer: "BE012"
+    }
+
 ]
 
 var testAPI = [
     {ondernemingsnummer:"test1",
      naam:'FC Barcelona',
-     omzet:'30000',
-     opbrengst:'50000'},
+     omzet:'39837,63',
+     opbrengst:'49987,71'},
 
     {ondernemingsnummer:"test2",
      naam:'Real VARdrid', 
-     omzet:'550000',
-     opbrengst:'790000'},
+     omzet:'5985313,64',
+     opbrengst:'790054,76'},
+
+     {ondernemingsnummer:"BE123",
+     naam:'CoolBlue', 
+     omzet:'124577,95',
+     opbrengst:'7956,31'},
+
+     {ondernemingsnummer:"BE456",
+     naam:'Bol.com', 
+     omzet:'550678,88',
+     opbrengst:'79999,99'},
+
+     {ondernemingsnummer:"BE789",
+     naam:'AP Hogeschool', 
+     omzet:'5507800,87',
+     opbrengst:'7978820,13'},
+
+     {ondernemingsnummer:"BE012",
+     naam:'iCoders', 
+     omzet:'769900751,99',
+     opbrengst:'2399876,55'},
     
 ]
 
@@ -29,20 +60,47 @@ function getInfo() {
 	var ondernemingsnummer2 = document.getElementById('ondernemingsnummer2').value
 
     var table = document.getElementById('api')
+    var counter1;
+    var counter2;
+ 
+
+    for(var k = 0; k< testAPI.length; k++){
+        
+        if(ondernemingsnummer1 == testAPI[k].ondernemingsnummer){
+        counter1 = 0
+    }
+        else if(ondernemingsnummer1 != testAPI[k].ondernemingsnummer){
+        counter1++;
+    }
+
+        if(ondernemingsnummer2 == testAPI[k].ondernemingsnummer){
+        counter2 = 0
+    }
+        else if(ondernemingsnummer2 != testAPI[k].ondernemingsnummer){
+        counter2++;
+        counter2 = counter2
+        
+    }}
+        
+    
+    
+
 
 	for(var i = 0; i < ondernemingsnummersAPI.length; i++) {
-		if(ondernemingsnummer1 == ondernemingsnummersAPI[i].ondernemingsnummer1 && ondernemingsnummer2 == ondernemingsnummersAPI[i].ondernemingsnummer2) {
+		if(ondernemingsnummer1 == ondernemingsnummersAPI[i].ondernemingsnummer && ondernemingsnummer2 != ondernemingsnummersAPI[i].ondernemingsnummer) {
 			console.log(" gegevens zijn juist ")
             
             var row = `<tr>
-							<th>${testAPI.indexOf({ondernemingsnummer} = ondernemingsnummer1).naam}:</th><br><br>
-							<th><p class="tabel" >omzet: <p>${testAPI[0].omzet}</th><br><br>
-							<th><p class="tabel" >opbrengst: <p>${testAPI[0].opbrengst}</th>
+							<td>${testAPI[counter1].naam}:</td><br><br>
+							<th><p class="tabel" >BE-nummer: <p>${testAPI[counter1].ondernemingsnummer}</th><br><br>
+                            <th><p class="tabel" >omzet: <p>${testAPI[counter1].omzet}</th><br><br>
+							<th><p class="tabel" >opbrengst: <p>${testAPI[counter1].opbrengst}</th>
 					  </tr>
                       <tr>
-							<th>${testAPI[1].naam}:</th><br><br>
-							<th><p class="tabel" >omzet: <p>${testAPI[1].omzet}</th><br><br>
-							<th><p class="tabel" >opbrengst: <p>${testAPI[1].opbrengst}</th>
+							<td>${testAPI[counter2].naam}:</td><br><br>
+                            <th><p class="tabel" >BE-nummer: <p>${testAPI[counter2].ondernemingsnummer}</th><br><br>
+							<th><p class="tabel" >omzet: <p>${testAPI[counter2].omzet}</th><br><br>
+							<th><p class="tabel" >opbrengst: <p>${testAPI[counter2].opbrengst}</th>
 					  </tr>`
 			table = document.getElementById("api").innerHTML += row
 			return
@@ -50,8 +108,9 @@ function getInfo() {
 		}
 
         else {
-            document.getElementById("api").innerHTML += "foute gegevens"
+            document.getElementById("errormessage").innerHTML = "foute gegevens";
+            console.log("foute gegevens")
         }
 	}
-	console.log("incorrect username or password")
+	
 }
